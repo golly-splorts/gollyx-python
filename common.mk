c31b1171ee5df179b218d0a84d9a7b16dc9dcc84
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-03-28 10:35:18 -0700

    more golly->gollyx fixes in common.mk

--- common.mk
+++ common.mk
@@ -1,5 +1,5 @@
 SHELL=/bin/bash
 
 ifeq ($(shell echo ${GOLLYX_PYTHON_HOME}),)
-$(error Environment variable GOLLYX_PYTHON_HOME not defined. Please run "source environment" in the golly-python repo root directory before running make commands)
+$(error Environment variable GOLLYX_PYTHON_HOME not defined. Please run "source environment" in the gollyx-python repo root directory before running make commands)
 endif
