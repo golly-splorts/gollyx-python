1f5dac22560accee7b1e890d42308199e444e024
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2022-01-22 18:19:53 -0800

    fix up examples

--- examples/bigmaps.py
+++ examples/bigmaps.py
@@ -1,7 +1,7 @@
-from golly_python.pylife import GOL
+from gollyx_python import HellmouthGOL
 
 def main():
-    gol = GOL(
+    gol = HellmouthGOL(
         s1 = '[{"49":[176,177,180,181,182]},{"50":[179]},{"51":[177]}]',
         s2 = '[{"149":[114]},{"150":[116]},{"151":[113,114,117,118,119]}]',
         rows=200,
@@ -12,10 +12,10 @@ def main():
         live_counts = gol.next_step()
         if gol.generation%500==0:
             print(f"Simulating generation {gol.generation}")
-            print(gol.get_live_counts())
+            print(gol.count())
 
     from pprint import pprint
-    pprint(gol.get_live_counts())
+    pprint(gol.count())
 
 if __name__=="__main__":
     main()
