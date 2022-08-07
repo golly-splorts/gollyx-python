5a94261e9feb58bc5f55977322d4bb8de8c87d72
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2022-08-06 20:01:51 -0700

    add PseudoGOL class

--- src/manager.py
+++ src/manager.py
@@ -9,11 +9,7 @@ from .starlife import StarBinaryGenerationsCA
 from .kleinlife import KleinBinaryLife
 
 
-# TODO: add PseudoGOL
-# re-use hellmouth binary life
-
-
-class ToroidalGOL(object):
+class HellmouthGOL(object):
     team_names: list = []
     columns = 0
     rows = 0
@@ -30,8 +26,8 @@ class ToroidalGOL(object):
         for i in range(self.rows):
             row = "|"
             for j in range(self.columns):
-                if self.life.is_alive(j, i):
-                    color = self.life.get_cell_color(j, i)
+                if self.life.actual_state.is_alive(j, i):
+                    color = self.life.actual_state.get_cell_color(j, i)
                     if color == 1:
                         row += "#"
                     elif color == 2:
@@ -94,12 +90,74 @@ class ToroidalGOL(object):
             self.halt = kwargs["halt"]
         else:
             self.halt = True
+        self.found_victor = False
 
         # Neighbor color legacy mode was used in Seasons 1-3
         if "neighbor_color_legacy_mode" in kwargs:
             self.neighbor_color_legacy_mode = kwargs["neighbor_color_legacy_mode"]
         else:
             self.neighbor_color_legacy_mode = False
+
+    def create_life(self):
+        try:
+            ic1 = json.loads(self.ic1)
+        except json.decoder.JSONDecodeError:
+            err = "Error: Could not load data as json:\n"
+            err += self.ic1
+            raise Exception(err)
+
+        try:
+            ic2 = json.loads(self.ic2)
+        except json.decoder.JSONDecodeError:
+            err = "Error: Could not load data as json:\n"
+            err += self.ic1
+            raise Exception(err)
+
+        self.life = HellmouthBinaryLife(
+            ic1,
+            ic2,
+            self.rows,
+            self.columns,
+            self.rule_b,
+            self.rule_s,
+            self.neighbor_color_legacy_mode,
+        )
+
+    def next_step(self):
+        return self.life.next_step()
+
+    def count(self):
+        return self.life.get_stats()
+
+    def check_for_victor(self):
+        return self.life.check_for_victor()
+
+    @property
+    def running(self):
+        return self.life.running
+
+    @property
+    def generation(self):
+        return self.life.generation
+
+
+class PseudoGOL(HellmouthGOL):
+    rule_b = [3, 5, 7]
+    rule_s = [2, 3, 8]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            rule_b=self.rule_b,
+            rule_s=self.rule_s,
+            neighbor_color_legacy_mode=False,
+            *args, **kwargs
+        )
+
+
+class ToroidalGOL(HellmouthGOL):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs, neighbor_color_legacy_mode=False)
 
     def create_life(self):
         try:
@@ -126,23 +184,6 @@ class ToroidalGOL(object):
             self.halt,
             self.neighbor_color_legacy_mode,
         )
-
-    def next_step(self):
-        return self.life.next_step()
-
-    def count(self):
-        return self.life.get_live_counts()
-
-    def check_for_victor(self):
-        return self.life.check_for_victor()
-
-    @property
-    def running(self):
-        return self.life.running
-
-    @property
-    def generation(self):
-        return self.life.generation
 
 
 class RainbowGOL(object):
@@ -272,138 +313,6 @@ class RainbowGOL(object):
 
     def check_for_victor(self):
         return self.life.found_victor
-
-    @property
-    def running(self):
-        return self.life.running
-
-    @property
-    def generation(self):
-        return self.life.generation
-
-
-class HellmouthGOL(object):
-    team_names: list = []
-    columns = 0
-    rows = 0
-    rule_b: list = []
-    rule_s: list = []
-
-    def __init__(self, **kwargs):
-        self.load_config(**kwargs)
-        self.create_life()
-
-    def __repr__(self):
-        s = []
-        s.append("+" + "-" * (self.columns) + "+")
-        for i in range(self.rows):
-            row = "|"
-            for j in range(self.columns):
-                if self.life.actual_state.is_alive(j, i):
-                    color = self.life.actual_state.get_cell_color(j, i)
-                    if color == 1:
-                        row += "#"
-                    elif color == 2:
-                        row += "o"
-                    else:
-                        row += "?"
-                else:
-                    row += "."
-            row += "|"
-            s.append(row)
-        s.append("+" + "-" * (self.columns) + "+")
-        rep = "\n".join(s)
-        rep += "\n"
-
-        livecounts = self.count()
-
-        rep += "\nGeneration: %d" % (self.generation)
-        rep += "\nLive cells, color 1: %d" % (livecounts["liveCells1"])
-        rep += "\nLive cells, color 2: %d" % (livecounts["liveCells2"])
-        rep += "\nLive cells, total: %d" % (livecounts["liveCells"])
-        rep += "\nVictory Percent: %0.1f %%" % (livecounts["victoryPct"])
-        rep += "\nCoverage: %0.2f %%" % (livecounts["coverage"])
-        rep += "\nTerritory, color 1: %0.2f %%" % (livecounts["territory1"])
-        rep += "\nTerritory, color 2: %0.2f %%" % (livecounts["territory2"])
-
-        return rep
-
-    def load_config(self, **kwargs):
-        """Load configuration from user-provided input params"""
-        if "s1" in kwargs and "s2" in kwargs:
-            self.ic1 = kwargs["s1"]
-            self.ic2 = kwargs["s2"]
-        else:
-            raise Exception("ERROR: s1 and s2 parameters must both be specified")
-
-        if "rows" in kwargs and "columns" in kwargs:
-            self.rows = kwargs["rows"]
-            self.columns = kwargs["columns"]
-        else:
-            raise Exception(
-                "ERROR: rows and columns parameters must be provided to GOL constructor"
-            )
-
-        if "rule_b" in kwargs:
-            self.rule_b = [int(j) for j in kwargs["rule_b"]]
-        else:
-            self.rule_b = [3]
-        if "rule_s" in kwargs:
-            self.rule_s = [int(j) for j in kwargs["rule_s"]]
-        else:
-            self.rule_s = [2, 3]
-
-        if "team1" in kwargs and "team2" in kwargs:
-            self.team_names = [kwargs["team1"], kwargs["team2"]]
-        else:
-            self.team_names = ["Team 1", "Team 2"]
-
-        # Whether to stop when a victor is detected
-        if "halt" in kwargs:
-            self.halt = kwargs["halt"]
-        else:
-            self.halt = True
-        self.found_victor = False
-
-        # Neighbor color legacy mode was used in Seasons 1-3
-        if "neighbor_color_legacy_mode" in kwargs:
-            self.neighbor_color_legacy_mode = kwargs["neighbor_color_legacy_mode"]
-        else:
-            self.neighbor_color_legacy_mode = False
-
-    def create_life(self):
-        try:
-            ic1 = json.loads(self.ic1)
-        except json.decoder.JSONDecodeError:
-            err = "Error: Could not load data as json:\n"
-            err += self.ic1
-            raise Exception(err)
-
-        try:
-            ic2 = json.loads(self.ic2)
-        except json.decoder.JSONDecodeError:
-            err = "Error: Could not load data as json:\n"
-            err += self.ic1
-            raise Exception(err)
-
-        self.life = HellmouthBinaryLife(
-            ic1,
-            ic2,
-            self.rows,
-            self.columns,
-            self.rule_b,
-            self.rule_s,
-            self.neighbor_color_legacy_mode,
-        )
-
-    def next_step(self):
-        return self.life.next_step()
-
-    def count(self):
-        return self.life.get_stats()
-
-    def check_for_victor(self):
-        return self.life.check_for_victor()
 
     @property
     def running(self):
@@ -559,7 +468,6 @@ class StarGOLGenerations(object):
     rule_c: list = []
     tol_zero_default: float = 1e-8
     tol_stable_default: float = 1e-6
-    running_avg_max_dim: int = 280
 
     def __init__(self, **kwargs):
         self.load_config(**kwargs)
@@ -709,9 +617,13 @@ class StarGOLGenerations(object):
 
 class KleinGOL(object):
 
+    team_names: list = []
+    columns = 0
+    rows = 0
+    rule_b: list = []
+    rule_s: list = []
     tol_zero_default: float = 1e-8
     tol_stable_default: float = 1e-8
-    running_avg_max_dim: int = 240
 
     def __init__(self, **kwargs):
         self.load_config(**kwargs)
