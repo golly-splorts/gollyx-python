01511501c6cc577a3d34fe58d5acb877137884cc
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2025-10-26 12:04:04 -0700

    update __init__ for api not manager

--- src/gollyx_python/__init__.py
+++ src/gollyx_python/__init__.py
@@ -1,11 +1,3 @@
-from .manager import (
-    ToroidalGOL, 
-    RainbowGOL, 
-    HellmouthGOL, 
-    DragonCA, 
-    StarGOLGenerations,
-    KleinGOL,
-) # noqa
-
+from .api import ToroidalGOL, StarGOL
 
 __version__ = "600.1.0"
